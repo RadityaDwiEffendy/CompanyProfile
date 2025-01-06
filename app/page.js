5845709cd2fb5aeb4f4545039c5a84f80892648c
RadityaDwiEffendy <effendyraditya11@gmail.com>
2025-01-06 14:07:16 +0700

    Add Animation

--- app/page.js
+++ app/page.js
@@ -28,6 +28,23 @@ export default function Navbar() {
     };
   }, []);
 
+  useEffect(() => {
+    const s = () => {
+      const z = document.getElementById('zoom-bg')
+      let sp = window.scrollY
+
+      z.style.backgroundSize = 100 + sp / 5 + "%";
+
+
+    }
+    window.addEventListener("scroll", s);
+
+    return () => {
+      window.removeEventListener("scroll", s)
+    }
+  }, [])
+
+  
 
 
   return (
@@ -35,7 +52,7 @@ export default function Navbar() {
       <nav className="bg-black shadow-md p-5 fixed z-[1000] top-0 left-0 w-full" >
 
         <ul className="flex space-x-10">
-          <img className="ml-16 w-[150px] h-16" src="/images/Logo_Gas.png"></img>
+          <Image className="ml-16 w-[150px] h-16" src="/images/Logo_Gas.png" alt="Logo Gas" width={150} height={64} />
 
           <div className="flex space-x-10 items-center ">
             <li>
@@ -69,9 +86,8 @@ export default function Navbar() {
             <div>
               <button
                 onClick={toggleDropdown}
-                className={`ml-10 w-24 h-10 border border-white ${
-                  isOpen ? "border-b-0" : ""
-                }`}>ID
+                className={`ml-10 w-24 h-10 border border-white ${isOpen ? "border-b-0" : ""
+                  }`}>ID
               </button>
               {isOpen && (
                 <div className="flex items-center justify-center ml-10 absolute w-24 h-10 border-l border-r border-b border-white">
@@ -86,9 +102,16 @@ export default function Navbar() {
         </ul>
       </nav>
 
-      <div className="w-full h-[100vh] bg-white ">
-        <div className="">
-          <p>Standar Baru dalam Manajemen Layanan & Operasi TI</p>
+      <div className="mt-[100px] w-full h-[110vh] bg-[url('/images/bgUngu.png')] bg-cover bg-center" id="zoom-bg">
+        <div className="absolute top-[50%] w-7/12 ml-10" >
+          <div className="">
+            <p className="text-7xl leading-tight font-medium">Standar Baru</p>
+            <p className="text-7xl leading-tight font-medium">dalam Manajemen</p>
+            <p className="text-7xl leading-tight font-medium">Layanan & Operasi TI</p>
+          </div>
+          <div className="w-[50rem] mt-10">
+            <p className="text-3xl">Memberikan Solusi TI yang inovatif dan efisien, memastikan bisnis Anda tetap terdepan di dunia digital yang bergerak cepat saat ini</p>
+          </div>
         </div>
       </div>
 
@@ -97,3 +120,5 @@ export default function Navbar() {
 
   );
 }
+
+
